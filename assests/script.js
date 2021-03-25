dc3185d40187a45ab7c41c0ce6090ab88fef6252
Drabis <drabis47@gmail.com>
2021-03-24 21:22:49 -0400

    update the layout

---
+++ assests/script.js
@@ -0,0 +1,11 @@
+// document.querySelector('#myList');
+
+// function mylist (){
+// var tools = document.createElement('li');
+// var toolsName = document.createElement('HTML');
+// tools.appendChild('HTML')
+// mylist();
+// }
+var popover = new bootstrap.Popover(document.querySelector('.popover-dismiss'), {
+    trigger: 'focus'
+  })
