f8c1a579be3c49e31aca75b4b03920d545cdf203
Drabis <drabis47@gmail.com>
2021-03-25 23:14:48 -0400

    add icons

--- assests/script.js
+++ assests/script.js
@@ -1,11 +1,4 @@
 // document.querySelector('#myList');
-
-// function mylist (){
-// var tools = document.createElement('li');
-// var toolsName = document.createElement('HTML');
-// tools.appendChild('HTML')
-// mylist();
-// }
 var popover = new bootstrap.Popover(document.querySelector('.popover-dismiss'), {
     trigger: 'focus'
   })
