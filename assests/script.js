4602150eb9959b077ce5458d4aa91966cc25d86e
Drabis <drabis47@gmail.com>
2021-03-27 00:04:56 -0400

    poject link on html

--- assests/script.js
+++ assests/script.js
@@ -5,6 +5,6 @@
 var popover = new bootstrap.Popover(document.querySelector('.popover-dismiss'), {
   trigger: 'focus'
 })
-var popover = new bootstrap.Popover(document.querySelector('.popover-dismiss'), {
-  trigger: 'focus'
-})
+var btnEl = document.querySelector('.btn');
+
+<button onclick="window.location.href='https://drabis.github.io/nba-game-locator/'">Continue</button>
